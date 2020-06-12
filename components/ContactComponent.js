31353de3d4d7ee1583a077c06e15b0c8ff37d534
Siddhant Chopra <siddhant.chopra@webdunia.net>
2020-06-13 00:20:25 +0530

    React navigation with About, contact page implemented

---
+++ components/ContactComponent.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import {Text, View, StyleSheet } from 'react-native'
+import { Card } from 'react-native-elements'
+
+class Contact extends Component{
+
+    render(){
+        return(
+            <View>
+                <Card
+                title="Contact Information"
+                >
+                    <Text style={styles.myState}>Our Address</Text>
+                <Text style={styles.myState}>121, Clear Water Bay Road</Text>
+                <Text style={styles.myState}>Clear Water Bay, Kowloon</Text>
+                <Text style={styles.myState}>HONG KONG</Text>
+                <Text style={styles.myState}>Tel: +852 1234 5678</Text>
+                <Text style={styles.myState}>Fax: +852 8765 4321</Text>
+                <Text style={styles.myState}>Email:confusion@food.net</Text>
+                </Card>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create ({
+    myState: {
+       marginTop: 10,
+       fontWeight: 'bold',      
+    }
+ })
+
+export default Contact
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Menu from './MenuComponent'
 import Home from './HomeComponent'
+import Contact from './ContactComponent'
+import About from './AboutComponent'
 import DishDetail from './DishDetailComponent'
 import { View, Platform } from 'react-native'
 import {createStackNavigator, createDrawerNavigator} from 'react-navigation'
@@ -37,6 +39,36 @@ const HomeNavigator = createStackNavigator({
     }
 })
 
+const ContactNavigator = createStackNavigator({
+    Contact: {screen: Contact},
+}, {
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor:'#512DAB'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+
+        }
+    }
+})
+
+const AboutNavigator = createStackNavigator({
+    About: {screen: About},
+}, {
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor:'#512DAB'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+
+        }
+    }
+})
+
 const MainNavigator = createDrawerNavigator({
     Home: {
         screen: HomeNavigator,
@@ -45,13 +77,28 @@ const MainNavigator = createDrawerNavigator({
             drawLabel: 'Home'
         }
     },
+    About: {
+        screen: AboutNavigator,
+        navigationOptions: {
+            title: 'About Us',
+            drawLabel: 'About Us'
+        }
+    },
     Menu: {
         screen: MenuNavigator,
         navigationOptions: {
             title: 'Menu',
             drawLabel: 'Menu'
         }
+    },
+    Contact: {
+        screen: ContactNavigator,
+        navigationOptions: {
+            title: 'Contact Us',
+            drawLabel: 'Contact Us'
+        }
     }
+  
 },{
     drawBackgroundColor: '#D1C4E9'
 })
